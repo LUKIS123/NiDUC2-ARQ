ead004d8920bf5471199e3729fa9e0ff10f99b05
Lukasz Gawron <51411768+LUKIS123@users.noreply.github.com>
2023-04-22 01:20:30 +0200

    console run update

--- mainApp.py
+++ mainApp.py
@@ -1,17 +1,19 @@
 import sys
 from threading import Thread
+
 import PIL.Image as Image
 
-import DataGenerator
+import ByteUtils
 from Channel import Channel
 from Enums.EncodingTypeEnum import EncodingType
 from Enums.NoiseTypeEnum import NoiseType
 from Receiver import Receiver
 from Sender import Sender
-import ByteUtils
 
 # ["generate", how_many_bytes]
-# ["run", how_many_frames, channel_noise, coding_type]
+
+# ["run", how_many_frames, channel_noise, coding_type, error_probability_of_good_state,
+# error_probability_of_bad_state, switch_to_good_probability, switch_to_bad_probability]
 
 print('Argument List:', str(sys.argv))
 
@@ -24,8 +26,10 @@ elif str(sys.argv[1]) == "run":
     if len(input_data) % int(sys.argv[2]) != 0:
         print("Invalid frame number given")
         sys.exit()
+
     data_sequences = int(sys.argv[2])
     frame_length = int(len(input_data) / data_sequences)
+
     src_frames = ByteUtils.separate_list_to_chunks(input_data, frame_length)
     print("Single frame length = ", str(len(src_frames[0])))
     src_hash = ByteUtils.calculate_md5_hash(ByteUtils.binary_to_byte_arr(ByteUtils.flatten_2d_list(src_frames)))
@@ -42,7 +46,8 @@ elif str(sys.argv[1]) == "run":
         print("Invalid noise type...")
         print("Proceeding with BSC channel...")
         ch_noise_type = NoiseType.bsc_channel
-    coding_type = str(sys.argv[3])
+    coding_type = str(sys.argv[4])
+
     match coding_type.lower():
         case "parity":
             coding_type = EncodingType.ParityBit
@@ -55,7 +60,25 @@ elif str(sys.argv[1]) == "run":
             print("Proceeding with parity bit coding...")
             coding_type = EncodingType.ParityBit
 
+    try:
+        p1 = float(sys.argv[5])
+    except IndexError:
+        p1 = 0.01
+    try:
+        p2 = float(sys.argv[6])
+    except IndexError:
+        p2 = 0.1
+    try:
+        p3 = float(sys.argv[7])
+    except IndexError:
+        p3 = 10
+    try:
+        p4 = float(sys.argv[8])
+    except IndexError:
+        p4 = 10
+
     channel = Channel(ch_noise_type)
+    channel.set_probabilities_gilbert_elliot(p1, p2, p3, p4)
     sender = Sender(src_frames, channel, coding_type, coding_type, 16)
     receiver = Receiver(channel, coding_type, coding_type, 16)
 
@@ -70,6 +93,7 @@ elif str(sys.argv[1]) == "run":
     print("Threads finished... Exiting...")
     print("Printing output data...")
     out_frames = receiver.output_bit_data_list_2d
+
     print(out_frames)
     print("MD5 comparison...")
     out_hash = ByteUtils.calculate_md5_hash(ByteUtils.binary_to_byte_arr(ByteUtils.flatten_2d_list(out_frames)))
@@ -94,68 +118,9 @@ elif str(sys.argv[1]) == "run":
 
     sys.exit()
 
-# ================================
-# image width == frame quantity
-data_sequences = 160
-# image height == frame length
-single_sequence_length = 8000
-
 # =========== ARQ TEST ===========
 # TODO: jedna ramka 100 bajtow czyli 800bit + naglowek i stopka
 #   do zrobienia naglowek ramki z numerowaniem
 # TODO: Zrobic aby wiadomosc ACK przesylala ktora rameczke chce z powrotem
 #   (aktualnie rozwiazanie powoduje zapetlanie sie jesli w jednym w odbiornikow przeskoczy indeks)
 #   obsluzyc przypadek gdy ilosc ramek przekroczy rozmiar naglowka
-
-data = DataGenerator.generate_bit_data(data_sequences, single_sequence_length)
-# md5 poczatkowe
-src_hash = ByteUtils.calculate_md5_hash(ByteUtils.binary_to_byte_arr(ByteUtils.flatten_2d_list(data)))
-
-print("Printing original data...")
-print(data)
-print("\n")
-
-channel = Channel(NoiseType.gilbert_elliot)
-
-# sender = Sender(data, channel, EncodingType.ParityBit, EncodingType.ParityBit, 16)
-# receiver = Receiver(channel, EncodingType.ParityBit, EncodingType.ParityBit, 16)
-# sender = Sender(data, channel, EncodingType.CRC_32, EncodingType.CRC_32, 16)
-# receiver = Receiver(channel, EncodingType.CRC_32, EncodingType.CRC_32, 16)
-sender = Sender(data, channel, EncodingType.CRC_8, EncodingType.CRC_8, 16)
-receiver = Receiver(channel, EncodingType.CRC_8, EncodingType.CRC_8, 16)
-
-sender_thread = Thread(target=sender.threaded_sender_function)
-receiver_thread = Thread(target=receiver.threaded_receiver_function, args=(len(data), 4))
-sender_thread.start()
-receiver_thread.start()
-
-# shutting down threads
-receiver_thread.join()
-sender_thread.join()
-print("Threads finished... Exiting...")
-
-# generating images
-img = Image.new('1', (data_sequences, single_sequence_length))
-pixels = img.load()
-for i in range(img.size[0]):
-    for j in range(img.size[1]):
-        pixels[i, j] = data[i][j]
-img.save('./pictures/original_image.bmp')
-
-out = receiver.output_bit_data_list_2d
-
-img_after = Image.new('1', (data_sequences, single_sequence_length))
-pixels_after = img_after.load()
-for i in range(img_after.size[0]):
-    for j in range(img_after.size[1]):
-        pixels_after[i, j] = out[i][j]
-img_after.save('./pictures/decoded_image.bmp')
-
-print("MD5...")
-# md5 poczatkowe
-out_hash = ByteUtils.calculate_md5_hash(ByteUtils.binary_to_byte_arr(ByteUtils.flatten_2d_list(out)))
-print(src_hash)
-print(out_hash)
-print(src_hash == out_hash)
-
-sys.exit()
